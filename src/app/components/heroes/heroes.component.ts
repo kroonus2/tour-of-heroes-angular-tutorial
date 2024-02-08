359df60b538e09247cf6d909a3e627732984bac7
kroonus2 <87575645+kroonus2@users.noreply.github.com>
2024-02-08 13:49:45 -0300

    update async subscribe service with observable

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -2,19 +2,38 @@ import { Component } from '@angular/core';
 import { Hero } from '../../interfaces/hero';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { HEROES } from '../../interfaces/mock-heroes';
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
+import { HeroService } from '../../service/hero.service';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
   imports: [
     CommonModule,
-    FormsModule],
+    FormsModule,
+    HeroDetailComponent],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
   }
+
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+
 }
+
