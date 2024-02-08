0922da32ed71c332e5198e5cbbcaf72dc0dcac22
kroonus2 <87575645+kroonus2@users.noreply.github.com>
2024-02-08 13:47:53 -0300

    create and implements hero details component

---
+++ src/app/components/hero-detail/hero-detail.component.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Hero } from '../../interfaces/hero';
+
+@Component({
+  selector: 'app-hero-detail',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './hero-detail.component.html',
+  styleUrl: './hero-detail.component.css'
+})
+export class HeroDetailComponent {
+
+  @Input() hero?: Hero;
+}
